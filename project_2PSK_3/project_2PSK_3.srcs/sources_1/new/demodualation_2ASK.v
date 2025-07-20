2b297c17dcdad8a0d3c852de99e6b31931d5c896
ChenYu Zhu <2792357190@qq.com>
2025-07-20 16:30:07 +0800

    generated bitstream

---
+++ project_2PSK_3/project_2PSK_3.srcs/sources_1/new/demodualation_2ASK.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/07/20 16:09:28
+// Design Name: 
+// Module Name: demodualation_2ASK
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module demodualation_2ASK(
+
+    );
+endmodule
